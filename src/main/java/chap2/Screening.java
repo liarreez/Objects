8e8de7ac7cfbd932490d62bde8774e87145e3dc4
liarreez <liarreez@gmail.com>
2023-03-03 19:20:23 +0900

    feat(Screening) : add Screening class

---
+++ src/main/java/chap2/Screening.java
@@ -0,0 +1,27 @@
+package chap2;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Screening(Moive movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public LocalDateTime getStartTime() {
+        return whenScreened;
+    }
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public Money getMovieFee() {
+        return movie.getFee();
+    }
+}
