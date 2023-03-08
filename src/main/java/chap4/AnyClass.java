841337f9ea84eabdec6104204bbbdb522467c799
liarreez <liarreez@gmail.com>
2023-03-08 22:09:51 +0900

    feat(Rectangle) : add enlarge feature

---
+++ src/main/java/chap4/AnyClass.java
@@ -0,0 +1,10 @@
+package chap4;
+
+public class AnyClass {
+    void anyMethod(Rectangle rectangle, int multiple) {
+        rectangle.setRight(rectangle.getRight() * multiple);
+        rectangle.setBottom(rectangle.getBottom() * multiple);
+        rectangle.setLeft(rectangle.getLeft() * multiple);
+        rectangle.setTop(rectangle.getTop() * multiple);
+    }
+}
