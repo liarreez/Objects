5e51f92f47d54519d16f9cb8dffad52f3f335b46
liarreez <liarreez@gmail.com>
2023-03-12 15:14:44 +0900

    feat(Screening) : add Screening class

---
+++ src/main/java/chap5/Screening.java
@@ -0,0 +1,18 @@
+package chap5;
+
+import chap4.Reservation;
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audieceCount);
+    }
+}
